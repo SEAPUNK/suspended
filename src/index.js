458d84173379df02a4b73eb714ca2f337d14d948
Ivan <ivan@sq10.net>
2015-11-30 12:20:04 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+require('babel-polyfill')
+
 class Test {
-  construct (name, fn) {
+  constructor (name, fn) {
     this.name = name
     this.fn = fn
     this.data = {}
@@ -20,18 +22,8 @@ class Suspended {
    * Constructs a Suspended instance.
    * @return {Suspended} Suspended instance
    */
-  construct () {
+  constructor () {
     this.tests = []
-  }
-
-  /**
-   * Sets up the tests with initial data.
-   * @param  {Object} data Data to use for the tests.
-   */
-  setup (data) {
-    this.tests.forEach(function (test) {
-      test.setup(data)
-    })
   }
 
   /**
@@ -45,23 +37,24 @@ class Suspended {
 
   /**
    * Runs the added tests.
+   * @param {Object} [data] Data to set up the tests with before running
    * @return {String} A failed test's name, if any.
    */
-  run () {
-    let self = this
-    return new Promise(async function (resolve, reject) {
-      for (let test of self.tests) {
-        let promise = test.run()
-        if (!test.fn || !test.fn.then) {
-          return reject(new Error(`test '${test.name}' does not return a promise`))
-        }
-        try {
-          await promise
-        } catch (err) {
-          resolve(test.name)
-        }
+  async run (data) {
+    for (let test of this.tests) {
+      test.setup(data || {})
+      let promise = test.run()
+      if (!promise || !promise.then) {
+        throw new Error(`test '${test.name}' does not return a promise`)
       }
-    })
+      try {
+        await promise
+        continue
+      } catch (err) {
+        return test.name
+      }
+    }
+    return null
   }
 }
 
